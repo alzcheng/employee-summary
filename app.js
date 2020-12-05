969e84c4d399684d03ed5492845e994b871290fc
AZC <albertzcheng@gmail.com>
2020-12-05 13:33:25 -0800

    basic logic is completed.  Want to refractor to get code organized to another js library

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const TeamGenerator = require("./lib/TeamGenerator");
+//const TeamGenerator = require("./lib/TeamGenerator");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -10,6 +10,184 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+
+// Creating TeamGenerator class that I want to refractor
+// TeamGenerator class gather information about the development team members,
+// and create objects for each team member.
+
+// After the user has input all employees desired, it calls render to create
+// a sting of html and then uses fs.writeFile to write the HTML 'team.html' in
+// the output folder.
+
+class TeamGenerator {
+  constructor() {
+    this.team = [];
+  }
+
+  start() {
+    this.askForNewMember();
+  }
+
+  askForNewMember() {
+    inquirer
+      .prompt([
+        {
+          type: "confirm",
+          name: "newMember",
+          message: "Do you want to add a new member to your team?",
+        },
+      ])
+      .then((answers) => {
+        //test if team is empty
+        //if team is not empty, generate file
+        if (answers.newMember) {
+          this.askForMemberType();
+        } else {
+          this.continue = false;
+          //this.quit();
+          fs.writeFile(outputPath, render(this.team), (err) => {
+            if (err) {
+              console.error(err);
+              return;
+            }
+          });
+          console.log("done");
+        }
+      });
+  }
+
+  askForMemberType() {
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "memberType",
+          message: "What type of team member would you like to add?",
+          choices: ["Manager", "Engineer", "Intern"],
+        },
+      ])
+      .then((answers) => {
+        if (answers.memberType === "Manager") {
+          this.createManager();
+        } else if (answers.memberType === "Engineer") {
+          this.createEngineer();
+        } else {
+          this.createIntern();
+        }
+      });
+  }
+
+  createManager() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "name",
+          message: "What is the Manager's name?",
+        },
+        {
+          type: "input",
+          name: "id",
+          message: "What is the Manager's ID number?",
+        },
+        {
+          type: "input",
+          name: "email",
+          message: "What is the Manager's email?",
+        },
+        {
+          type: "input",
+          name: "officeNumber",
+          message: "What is the Manager's office number?",
+        },
+      ])
+      .then((answers) => {
+        const NewManager = new Manager(
+          answers.name,
+          answers.id,
+          answers.email,
+          answers.officeNumber
+        );
+        this.team.push(NewManager);
+        console.log("Manager Created");
+        this.askForNewMember();
+      });
+  }
+
+  createEngineer() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "name",
+          message: "What is the Engineer's name?",
+        },
+        {
+          type: "input",
+          name: "id",
+          message: "What is the Engineer's ID number?",
+        },
+        {
+          type: "input",
+          name: "email",
+          message: "What is the Engineer's email?",
+        },
+        {
+          type: "input",
+          name: "github",
+          message: "What is the Engineer's Github username?",
+        },
+      ])
+      .then((answers) => {
+        const NewEngineer = new Engineer(
+          answers.name,
+          answers.id,
+          answers.email,
+          answers.github
+        );
+        this.team.push(NewEngineer);
+        console.log("Engineer Created");
+        this.askForNewMember();
+      });
+  }
+
+  createIntern() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "name",
+          message: "What is the Intern's name?",
+        },
+        {
+          type: "input",
+          name: "id",
+          message: "What is the Intern's ID number?",
+        },
+        {
+          type: "input",
+          name: "email",
+          message: "What is the Intern's email?",
+        },
+        {
+          type: "input",
+          name: "school",
+          message: "What is the Intern's school?",
+        },
+      ])
+      .then((answers) => {
+        const NewIntern = new Intern(
+          answers.name,
+          answers.id,
+          answers.email,
+          answers.school
+        );
+        this.team.push(NewIntern);
+        console.log("Intern Created");
+        this.askForNewMember();
+      });
+  }
+}
 
 const myTeam = new TeamGenerator();
 myTeam.start();
@@ -21,28 +199,6 @@ myTeam.start();
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
 
-const test1 = new Manager("far", 2323, "aasdf", 123);
-const test2 = new Engineer("near", 23423, "asdfa", "aedfad");
-const test3 = new Intern("wherever", 1232, "adfas", "asdfas");
-const testArr = [test1, test2, test3];
-
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-fs.writeFile(outputPath, render(testArr), (err) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-});
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
